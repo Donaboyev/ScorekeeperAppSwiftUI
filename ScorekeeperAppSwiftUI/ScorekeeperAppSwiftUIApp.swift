0922f9ed428fc6022b55cddd3225311a4234d782
Abbos Donaboyev <donaboyevabbos1701@gmail.com>
2023-12-25 10:39:49 +0500

    Initial Commit

---
+++ ScorekeeperAppSwiftUI/ScorekeeperAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScorekeeperAppSwiftUIApp.swift
+//  ScorekeeperAppSwiftUI
+//
+//  Created by Abbos Donaboev on 25/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ScorekeeperAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
